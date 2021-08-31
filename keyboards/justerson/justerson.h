058856e98d043c2f2e97ff492e64d763910277b1
Justin Anderson-Pomeroy <justerson@gmail.com>
2021-08-31 09:29:37 +0100

    commas again, back to haunt me

--- keyboards/justerson/justerson.h
+++ keyboards/justerson/justerson.h
@@ -10,27 +10,28 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define ___ KC_NO
+
+#define ____
 
 #define LAYOUT( \
-    LH00, LH01, LH02, LH03, LH04, LH05, LH06, LH07,    RH00, RH01, RH02, RH03, RH04, RH05, RH06, RH07 \
-    LH10, LH11, LH12, LH13, LH14, LH15, LH16,          RH10, RH11, RH12, RH13, RH14, RH15, RH16, RH17 \
-    LH20, LH21, LH22, LH23, LH24, LH25,       LH27,    RH20, RH21, RH22, RH23, RH24, RH25,       RH27 \
-    LH30, LH31, LH32, LH33, LH34, LH35, LH36, LH37,    RH30, RH31, RH32,       RH34, RH35, RH36, RH37 \
+    LH00, LH01, LH02, LH03, LH04, LH05, LH06, LH07,    RH00, RH01, RH02, RH03, RH04, RH05, RH06, RH07, \
+    LH10, LH11, LH12, LH13, LH14, LH15, LH16,          RH10, RH11, RH12, RH13, RH14, RH15, RH16, RH17, \
+    LH20, LH21, LH22, LH23, LH24, LH25,       LH27,    RH20, RH21, RH22, RH23, RH24, RH25,       RH27, \
+    LH30, LH31, LH32, LH33, LH34, LH35, LH36, LH37,    RH30, RH31, RH32,       RH34, RH35, RH36, RH37, \
     LH40, LH41,       LH43, LH44,       LH46,          RH40,       RH42,       RH44, RH45, RH46, RH47 \
 ) \
 { \
-    { LH00, LH01, LH02, LH03, LH04, LH05, LH06, LH07}, \
-    { LH10, LH11, LH12, LH13, LH14, LH15, LH16, ____}, \
-    { LH20, LH21, LH22, LH23, LH24, LH25, ____, LH27}, \
-    { LH30, LH31, LH32, LH33, LH34, LH35, LH36, LH37}, \
-    { LH40, LH41, ____, LH43, LH44, ____, LH46, ____}, \
+    { LH00, LH01, LH02, LH03, LH04, LH05, LH06, LH07 }, \
+    { LH10, LH11, LH12, LH13, LH14, LH15, LH16, ____ }, \
+    { LH20, LH21, LH22, LH23, LH24, LH25, ____, LH27 }, \
+    { LH30, LH31, LH32, LH33, LH34, LH35, LH36, LH37 }, \
+    { LH40, LH41, ____, LH43, LH44, ____, LH46, ____ }, \
 \
-    { RH00, RH01, RH02, RH03, RH04, RH05, RH06, RH07}, \
-    { RH10, RH11, RH12, RH13, RH14, RH15, RH16, RH17}, \
-    { RH20, RH21, RH22, RH23, RH24, RH25, ____, RH27}, \
-    { RH30, RH31, RH32, ____, RH34, RH35, RH36, RH37}, \
-    { RH40, ____, RH42, ____, RH44, RH45, RH46, RH47}  \
+    { RH00, RH01, RH02, RH03, RH04, RH05, RH06, RH07 }, \
+    { RH10, RH11, RH12, RH13, RH14, RH15, RH16, RH17 }, \
+    { RH20, RH21, RH22, RH23, RH24, RH25, ____, RH27 }, \
+    { RH30, RH31, RH32, ____, RH34, RH35, RH36, RH37 }, \
+    { RH40, ____, RH42, ____, RH44, RH45, RH46, RH47 }  \
 }
 
 /* LAYOUT

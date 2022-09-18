a270efe171ef593336f5b53a332f492d8c504e53
Justin Anderson-Pomeroy <justerson@gmail.com>
2022-09-18 19:10:51 +0100

    swap delete, backspace

--- keyboards/justerson/keymaps/justerson/keymap.c
+++ keyboards/justerson/keymaps/justerson/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
         KC_ESC,   KC_GRV,   KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    // L1
-        KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, // R1
+        KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  // R1
         KC_CAPS,  KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,             // L2
         KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, // R2
         KC_UP,    KC_ENT,   KC_A,    KC_S,     KC_D,    KC_F,             KC_G,    // L3

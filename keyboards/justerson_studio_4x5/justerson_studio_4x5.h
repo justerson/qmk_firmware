f8fc190926ea967e10dc4ab15e640033fb3f3b67
Justin Anderson-Pomeroy <justin.anderson@washpost.com>
2025-03-23 19:18:54 +0000

    update to new qmk

--- keyboards/justerson_studio_4x5/justerson_studio_4x5.h
+++ keyboards/justerson_studio_4x5/justerson_studio_4x5.h
@@ -8,15 +8,3 @@
  * $ qmk flash -kb <my_keyboard> -km <my_keymap>
  *
  */
-
-#define LAYOUT( \
-  K00, K01, K02, K03, K04, \
-  K10, K11, K12, K13, K14, \
-  K20, K21, K22, K23, K24, \
-  K30, K31, K32, K33, K34  \
-) { \
-  { K00, K01, K02, K03, K04 }, \
-  { K10, K11, K12, K13, K14 }, \
-  { K20, K21, K22, K23, K24 }, \
-  { K30, K31, K32, K33, K34 }  \
-}
--- keyboards/justerson_studio_4x5/keymaps/studio_4x5/keymap.c
+++ keyboards/justerson_studio_4x5/keymaps/studio_4x5/keymap.c
@@ -1,29 +1,21 @@
 #include QMK_KEYBOARD_H
 
-// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-// 	[0] = LAYOUT(
-//         KC_A,	KC_B,	KC_C,	KC_D,    KC_E,
-//         KC_F,	KC_G,	KC_H,	KC_I,    KC_J,
-//         KC_K,	KC_L,	KC_M,   KC_N,    KC_O,
-//         KC_P,	KC_Q,	KC_R,   KC_S,    KC_T
-//     ),
-// };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(
+    /*
+     * ┌───────┬───────┬───────┬───────┬───────┐
+     * │ SPACE │ CMD-Z │ LEFT  │   A   │  ESC  │
+     * ├───────┼───────┼───────┼───────┤───────┤
+     * │ CTRL  │ CMD-X │ UP    │   W   │ CMD-S │
+     * ├───────┼───────┼───────┼───────┤───────┤
+     * │ CMD   │ CMD-V │ DOWN  │   S   │ CMD-A │
+     * ├───────┼───────┼───────┼───────┤───────┤
+     * │ BACK  │ CMD-C │ RIGHT │   D   │ ENTER │
+     * └───────┴───────┴───────┴───────┴───────┘
+     */
+    [0] = LAYOUT(
         KC_SPC,     LGUI(KC_Z),	    KC_LEFT,    KC_A,    KC_ESCAPE,
-        LCTL(),	    LGUI(KC_X),	    KC_UP,      KC_W,    LGUI(KC_S),
-        LGUI(),	    LGUI(KC_V),	    KC_DOWN,    KC_S,    LGUI(KC_A),
-        KC_DELETE,	LGUI(KC_C),	    KC_RIGHT,   KC_D,    KC_ENTER
+        KC_LCTL,    LGUI(KC_X),	    KC_UP,      KC_W,    LGUI(KC_S),
+        KC_LCMD,    LGUI(KC_V),	    KC_DOWN,    KC_S,    LGUI(KC_A),
+        KC_BSPC,	LGUI(KC_C),	    KC_RIGHT,   KC_D,    KC_ENTER
     ),
 };
-// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-// 	[0] = LAYOUT(
-//         KC_UP,	    LCTL(KC_LEFT),	KC_WAKE,	       LGUI(KC_Z), KC_BSPC,
-//         KC_DOWN,	LCTL(KC_RIGHT),	KC_KB_MUTE,	       LGUI(KC_X), KC_DELETE,
-//         KC_ENTER,	KC_HOME,	    KC_KB_VOLUME_UP,   LGUI(KC_P), KC_ENTER,
-//         LGUI(KC_K),	KC_END,	        KC_KB_VOLUME_DOWN, LGUI(KC_C), KC_ESCAPE
-//     ),
-// };
-
-
